052cb10053f15d6f8ae507c0edf0f66cd88a455c
Tuyet Le <tle83@lsu.edu>
2017-02-15 12:53:41 -0600

    Effect Buttons Added

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 var synth;
 var player1, player2, player3, player4;
-var distorty;
 var s1 = false;
 var s2 = false;
 var s3 = false;
 var s4 = false;
+var distorty;
 
 //test var
 var c;
@@ -15,7 +15,7 @@ function setup(){
 	synth = new Tone.Synth().toMaster();
 	//synth.triggerAttackRelease('C4', '8n');
 
-	//distory = new Tone.Distortion(0.8);s
+	//distory = new Tone.Distortion(0.8);
 	
 	var player1 = new Tone.Player("").toMaster();
 	var player2 = new Tone.Player("").toMaster();
@@ -45,19 +45,37 @@ function draw(){
 	if(s1 == true){
 		fill(150);
 		rect(125, 20, 450, 350);
+
+		effectButtons();
 	}
+
 	if(s2 == true){
 		fill(150);
 		rect(125, 20, 450, 350);
+
+		effectButtons();
 	}
+
 	if(s3 == true){
 		fill(150);
 		rect(125, 20, 450, 350);
+
+		effectButtons();
 	}
-	if(s3 == true){
+
+	if(s4 == true){
 		fill(150);
 		rect(125, 20, 450, 350);
+
+		effectButtons();
 	}
+}
+
+function effectButtons(){
+	button("Attack", 150, 60, 80, 50);
+	button("Decay", 150, 130, 80, 50);
+	button("Sustain", 150, 200, 80, 50);	
+	button("Release", 150, 270, 80, 50);
 }
 
 function button(name,x,y,width,height){
@@ -74,23 +92,35 @@ function mousePressed(){
 		(mouseY >= 20) && (mouseY <= 50)){
 			c = color(155, 0, 0);
 			s1 = true;
+			s2 = false;
+			s3 = false;
+			s4 = false;
 			synth.triggerAttackRelease(440, 0.1);		
 		}
 	else if((mouseX >= 20) && (mouseX <= 100) &&
 		(mouseY >= 60) && (mouseY <= 90)){
 			c = color(0, 155, 0);
+			s1 = false;
 			s2 = true;
+			s3 = false;
+			s4 = false;
 			synth.triggerAttackRelease(440, 0.1);
 		}
 	else if((mouseX >= 20) && (mouseX <= 100) &&
 		(mouseY >= 100) && (mouseY <= 130)){
 			c = color(0, 0, 155);
+			s1 = false;
+			s2 = false;
 			s3 = true;
+			s4 = false;
 			synth.triggerAttackRelease(440, 0.1);
 		}
 	else if((mouseX >= 20) && (mouseX <= 100) &&
 		(mouseY >= 140) && (mouseY <= 170)){
 			c = color(0, 255, 255);
+			s1 = false;
+			s2 = false;
+			s3 = false;
 			s4 = true;
 			synth.triggerAttackRelease(440, 0.1);
 		}
