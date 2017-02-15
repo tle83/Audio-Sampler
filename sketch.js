cb7068a968eaf6d38fc7dae06131aec8fdb7b002
Tuyet Le <tle83@lsu.edu>
2017-02-15 12:14:44 -0600

    States Added
    =4 states added for each sample
    =Sample buttons are added
    =“Stop” button added

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
 var synth;
 var player1, player2, player3, player4;
 var distorty;
+var s1 = false;
+var s2 = false;
+var s3 = false;
+var s4 = false;
+
+//test var
+var c;
 
 function setup(){
 	createCanvas(600, 400);
-	background(100, 100, 100);
+	c = color(100, 100, 100);
 	synth = new Tone.Synth().toMaster();
 	//synth.triggerAttackRelease('C4', '8n');
-	//synth.triggerAttackRelease(440, 2);
 
 	//distory = new Tone.Distortion(0.8);s
 	
@@ -29,10 +35,29 @@ function setup(){
 }
 
 function draw(){
-	button("Button 1", 20, 20, 80, 30);	
-	button("Button 2", 20, 60, 80, 30);
-	button("Button 3", 20, 100, 80, 30);
-	button("Button 4", 20, 140, 80, 30);
+	background(c);
+	button("Sample 1", 20, 20, 80, 30);	
+	button("Sample 2", 20, 60, 80, 30);
+	button("Sample 3", 20, 100, 80, 30);
+	button("Sample 4", 20, 140, 80, 30);
+	button("\t STOP", 20, 300, 80, 30);
+
+	if(s1 == true){
+		fill(150);
+		rect(125, 20, 450, 350);
+	}
+	if(s2 == true){
+		fill(150);
+		rect(125, 20, 450, 350);
+	}
+	if(s3 == true){
+		fill(150);
+		rect(125, 20, 450, 350);
+	}
+	if(s3 == true){
+		fill(150);
+		rect(125, 20, 450, 350);
+	}
 }
 
 function button(name,x,y,width,height){
@@ -41,5 +66,40 @@ function button(name,x,y,width,height){
 	rect(x,y,width,height);
 	noStroke();
 	fill(255);
-	text(name, x * 2, y + 20);
+	text(name, x * 1.7, y + 20);
+}
+
+function mousePressed(){
+	if((mouseX >= 20) && (mouseX <= 100) &&
+		(mouseY >= 20) && (mouseY <= 50)){
+			c = color(155, 0, 0);
+			s1 = true;
+			synth.triggerAttackRelease(440, 0.1);		
+		}
+	else if((mouseX >= 20) && (mouseX <= 100) &&
+		(mouseY >= 60) && (mouseY <= 90)){
+			c = color(0, 155, 0);
+			s2 = true;
+			synth.triggerAttackRelease(440, 0.1);
+		}
+	else if((mouseX >= 20) && (mouseX <= 100) &&
+		(mouseY >= 100) && (mouseY <= 130)){
+			c = color(0, 0, 155);
+			s3 = true;
+			synth.triggerAttackRelease(440, 0.1);
+		}
+	else if((mouseX >= 20) && (mouseX <= 100) &&
+		(mouseY >= 140) && (mouseY <= 170)){
+			c = color(0, 255, 255);
+			s4 = true;
+			synth.triggerAttackRelease(440, 0.1);
+		}
+	else if((mouseX >= 20) && (mouseX <= 100) &&
+		(mouseY >= 300) && (mouseY <= 330)){
+			s1 = false;
+			s2 = false;
+			s3 = false;
+			s4 = false;
+			c = color(100, 100, 100);
+		}
 }
